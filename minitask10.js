32988bf4bbf6d4910af19cb565c4d9238ae2d91f
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-27 11:25:25 +0700

    Update minitask10.js

--- minitask10.js
+++ minitask10.js
@@ -6,8 +6,8 @@ let angka2 = [1,2,3,4,5];
 let arrayKosong = [];
 let hasilBalik = 0;
 for(let i = angka2.length - 1; i >= 0;i--) {
-    let array = angka2[i];
-    arrayKosong[hasilBalik] = array;
+    let dataArray = angka2[i];
+    arrayKosong[hasilBalik] = dataArray;
      hasilBalik++;
 };
 console.log(arrayKosong);
